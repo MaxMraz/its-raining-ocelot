e3ff609e8ba4510cda1fece375d3110ee6b17515
WaynePL <wayneplivingston@gmail.com>
2018-11-16 21:02:01 -0500

    best version

---
+++ precache-manifest.84cea44235144d9490b148ea3b9f503d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "269d352bf296d3c059b96d47fca01ae5",
+    "url": "/its-raining-ocelot/static/media/tiger.269d352b.png"
+  },
+  {
+    "revision": "e3c618a5c89ffbb044ff8f9abe443f3c",
+    "url": "/its-raining-ocelot/static/media/snake.e3c618a5.png"
+  },
+  {
+    "revision": "563567b5dfe1acd56d546741cea0f827",
+    "url": "/its-raining-ocelot/static/media/ocedrop.563567b5.png"
+  },
+  {
+    "revision": "fa029af8e38a6c7435c82b0ac5ef3899",
+    "url": "/its-raining-ocelot/static/media/ghost.fa029af8.png"
+  },
+  {
+    "revision": "218c2688a28e2b70bd5323d6ef41b71a",
+    "url": "/its-raining-ocelot/static/media/bison.218c2688.png"
+  },
+  {
+    "revision": "9c3839481fd195aac3f9",
+    "url": "/its-raining-ocelot/static/js/runtime~main.9c383948.js"
+  },
+  {
+    "revision": "d2f1b1efe096cff1648c",
+    "url": "/its-raining-ocelot/static/js/main.d2f1b1ef.chunk.js"
+  },
+  {
+    "revision": "326adf9ffc629449a64c",
+    "url": "/its-raining-ocelot/static/js/1.326adf9f.chunk.js"
+  },
+  {
+    "revision": "d2f1b1efe096cff1648c",
+    "url": "/its-raining-ocelot/static/css/main.0f757d3a.chunk.css"
+  },
+  {
+    "revision": "5c651a340d3324ab546ab1a62fa3bd1f",
+    "url": "/its-raining-ocelot/index.html"
+  }
+];
